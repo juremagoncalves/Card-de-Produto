1323d5fec759f39a8c2b3aa92bba4a72a0e30f20
juremagoncalves <99494979+juremagoncalves@users.noreply.github.com>
2023-09-24 02:05:27 +0100

    Update main.js

--- main.js
+++ main.js
@@ -8,28 +8,21 @@ let troca = true
 
 function toggle(){
     if(troca){
-        console.log(troca)
         icon.querySelector(".start").classList.toggle("hide");
         icon.querySelector(".close").classList.toggle("hide");
         sofa.setAttribute("src","./assets/sofa.gif")
         troca = false;
-        console.log("entrei negativo = " + troca)
-       
     }
     else{
-        console.log(troca + "ttttttttt")
         icon.querySelector(".start").classList.toggle("hide");
         icon.querySelector(".close").classList.toggle("hide");
-        sofa.setAttribute("src","./assets/image 2.png")
+        sofa.setAttribute("src","assets/image 2.png")
         troca = true;
-       
-        console.log("entrei positivo" + troca)
     }
 }
 icon.addEventListener("click", toggle)
 
 document.addEventListener("click", () =>{
-    console.log('yes')
     setTimeout(function() {
         // icon.style.animation = ""
         icon.classList.toggle("show-background")
